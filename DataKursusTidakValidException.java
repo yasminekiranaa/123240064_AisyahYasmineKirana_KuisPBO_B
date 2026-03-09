3672e3752d4dfa07ac1bda8127686371eda663b1
Yasmine Kirana <aisyahyasmine96@gmail.com>
2026-03-09 14:13:03 +0630

    Add files via upload

---
+++ DataKursusTidakValidException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studikita;
+
+/**
+ *
+ * @author Lab Informatika
+ */
+public class DataKursusTidakValidException extends Exception {
+    public DataKursusTidakValidException(String message){
+        super(message);
+    }
+}
---
+++ KursusOffline.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studikita;
+
+/**
+ *
+ * @author Lab Informatika
+ */
+public class KursusOffline extends PaketKursus implements Perpanjang {
+    public KursusOffline(double hargaKursus) throws DataKursusTidakValidException{
+        super("Kursus Offline", hargaKursus);
+    }
+    
+    @Override
+    public double hitungTotalBiaya(){
+        return hargaKursus + 50000;
+    }
+    
+    @Override
+    public void cetakInfoPerpanjangan() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
